be493470e31b1824f42e84e70eae40ea5eff17d1
Grant West <grantwest62@yahoo.com>
2015-04-18 16:42:47 -0700

    Doctor Changes_2
    Removed redundant accessor and mutator methods, added patientList
    functionality

--- src/Doctor.java
+++ src/Doctor.java
@@ -7,7 +7,7 @@
 import java.util.*;
 public class Doctor extends User {
     private String name, email, phone, address, hospital, pass, secretQuestion, secretAnswer;
-    //private ArrayList<Patient> patientList;
+    private ArrayList<Patient> patientList;
     
     //constructor
    public Doctor(String docName, String docEmail,String docPhone, String docAddress, 
@@ -17,38 +17,14 @@ public class Doctor extends User {
         hospital = docHospital;
         secretQuestion = docSecret;
         secretAnswer = docAnswer;
-        //patientList = new ArrayList<Patient>();
+        patientList = new ArrayList<Patient>();
     }
     
    //accessor methods for all data
-    public String getName()
-    {
-        return name;
-    }
-    
-    public String getEmail()
-    {
-        return email;
-    }
-    
-    public String getAddress()
-    {
-        return address;
-    }
-    
-    public String getPhone()
-    {
-        return phone;
-    }
-    
+ 
     public String getHospital()
     {
         return hospital;
-    }
-    
-    public String getPass()
-    {
-        return pass;
     }
     public String getSecret()
     {
@@ -59,37 +35,17 @@ public class Doctor extends User {
     {
         return secretAnswer;
     }
-    
-    //mutator methods for all data
-     public void setName(String changeName)
+    public ArrayList<Patient> getPatientList()
     {
-        name = changeName;
+    	return patientList;
     }
     
-    public void setEmail(String changeEmail)
-    {
-        email = changeEmail;
-    }
-    
-    public void setAddress(String changeAddress)
-    {
-        address = changeAddress;
-    }
-    
-    public void setPhone(String changePhone)
-    {
-        phone = changePhone;
-    }
-    
+    //mutator methods
     public void setHospital(String changeHospital)
     {
         hospital = changeHospital;
     }
     
-    public void setPass(String changePass)
-    {
-        pass = changePass;
-    }
     public void setSecret(String changeSecret)
     {
         secretQuestion = changeSecret;
@@ -99,11 +55,12 @@ public class Doctor extends User {
     {
         secretAnswer = changeAnswer;
     }
+    public void registerPatient(Patient newPatient)
+    {
+    	patientList.add(newPatient);
+    }
 }
     
-    /*public void registerPatient(Patient addPatient)
-    {
-        patientList.add(new Patient(...));
-    }
+    
    
-}*/
+
