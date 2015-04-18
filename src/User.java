7e3f5ac4ff2ba44663f2d9c49ce8a46f0423d9c7
Grant West <grantwest62@yahoo.com>
2015-04-18 16:15:27 -0700

    User Changes
    changed several things about the user class

--- src/User.java
+++ src/User.java
@@ -22,85 +22,54 @@ public class User implements Serializable {
 //i named the class cereal because I have lame jokes
 //implements serializable so we can store users as objects and retrieve them
 //private vars for each var listed in user class diagram
-	private String firstName;
-	private String lastName;
-	private int phoneNumber;
+	private String name;
 	private String email;
-	private boolean authenticated;
-	private String username;
+	private String phoneNumber;
+	private String address;
 	private String password;
-	private User user;
 	
-	public User(String firstName, String lastName, String email, int phoneNumber, boolean authenticated, String username, String password){
+	public User(String name,  String email, String phoneNumber, String address, String password){
 //constructor method		
-		super();
-		this.firstName=firstName;
-		this.lastName=lastName;
+		this.name=name;
 		this.email=email;
 		this.phoneNumber=phoneNumber;
-		this.authenticated=authenticated;
-		this.username=username;
+		this.email=email;
 		this.password=password;
-		User user;
 	}
 //getter methods for each var	
-	public String getFirstName(){
-		return firstName;
+	public String getname(){
+		return name;
 	}
-	
-	public String getLastName(){
-		return lastName;
-	}
-	
 	public String getEmail(){
 		return email;
 	}
 	
-	public int getPhoneNumber(){
+	public String getPhoneNumber(){
 		return phoneNumber;
-	}
-	
-	public boolean getAuthenticated(){
-		return authenticated;
-	}
-	
-	public String getUserName(){
-		return username;
 	}
 	
 	public String getPassword(){
 		return password;
 	}
 //setter methods for each var	
-	public void setFirstName(){
-		this.firstName=firstName;
+	public void setName(String name){
+		this.name=name;
 	}
 	
-	public void setLastname(){
-		this.lastName=lastName;
-	}
-	
-	public void setEmail(){
+	public void setEmail(String email){
 		this.email=email;
 	}
 	
-	public void setPhoneNumber(){
+	public void setPhoneNumber(String phoneNumber){
 		this.phoneNumber=phoneNumber;
 	}
 	
-	public void setAuthenticated(){
-		this.authenticated=authenticated;
-	}
-	
-	public void setUsername(){
-		this.username=username;
-	}
-	
-	public void setPassword(){
+	public void setPassword(String Password){
 		this.password=password;
 	}
 //this is where we get the user object from the text file	
 	public User getUser(){
+		User user = null;
 		try{
 			FileInputStream fileInput= new FileInputStream("user.txt");
 			ObjectInputStream objectInput= new ObjectInputStream (fileInput);
